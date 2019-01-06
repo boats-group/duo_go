c9f4e3cf2c596c34512925b55b1917b201564e40
Rudy Puig <rudy.puig@boats.com>
2019-01-05 19:35:38 -0500

    Adding 100% coverage to duo/base plus minor changes

--- duo/base.go
+++ duo/base.go
@@ -5,12 +5,23 @@ import (
 	"crypto/sha1"
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"strconv"
 	"strings"
 	"time"
 
-	log "github.com/sirupsen/logrus"
+	logrus "github.com/sirupsen/logrus"
+)
+
+const (
+	errParsePayloadInvalidPayload    = "invalid payload format"
+	errParsePayloadInvalidSignature  = "signature hash does not match"
+	errParsePayloadInvalidPrefix     = "payload prefix does not match"
+	errParsePayloadInvalidContent    = "payload content is invalid"
+	errParsePayloadInvalidExpiration = "payload expiration is invalid"
+	errParsePayloadInvalidKey        = "payload key does not match"
+	errParsePayloadInvalidUsername   = "payload username is invalid"
 )
 
 var (
@@ -37,21 +48,21 @@ func hmacSha1(secret string, message string) string {
 }
 
 func signPayload(secret string, payload []string, prefix string, ttl int) string {
-	log.WithFields(log.Fields{
+	logrus.WithFields(logrus.Fields{
 		"secret":  secret,
 		"payload": strings.Join(payload, "|"),
 		"prefix":  prefix,
 		"ttl":     ttl,
 	}).Debug("signPayload:in")
 
-	payloadExpiration := strconv.FormatInt(time.Now().Local().Add(time.Second*time.Duration(ttl)).Unix(), 10)
-	content := strings.Join(append(payload, []string{payloadExpiration}...), "|")
+	expiration := strconv.FormatInt(time.Now().Local().Add(time.Second*time.Duration(ttl)).Unix(), 10)
+	content := strings.Join(append(payload, []string{expiration}...), "|")
 	encodedContent := base64.StdEncoding.EncodeToString([]byte(content))
 	cookie := fmt.Sprintf("%s|%s", prefix, encodedContent)
 	signature := hmacSha1(secret, cookie)
 
-	log.WithFields(log.Fields{
-		"expiration": payloadExpiration,
+	logrus.WithFields(logrus.Fields{
+		"expiration": expiration,
 		"content":    content,
 		"cookie":     cookie,
 		"signature":  signature,
@@ -61,7 +72,7 @@ func signPayload(secret string, payload []string, prefix string, ttl int) string
 }
 
 func parsePayload(secret string, payload string, prefix string, key string) (string, error) {
-	log.WithFields(log.Fields{
+	logrus.WithFields(logrus.Fields{
 		"secret":  secret,
 		"payload": payload,
 		"prefix":  prefix,
@@ -71,7 +82,7 @@ func parsePayload(secret string, payload string, prefix string, key string) (str
 	// Parse payload data
 	payloadValues := strings.Split(payload, "|")
 	if len(payloadValues) != 3 {
-		return "", fmt.Errorf("invalid payload format")
+		return "", errors.New(errParsePayloadInvalidPayload)
 	}
 
 	// Validate payload signature (HMAC-SHA1)
@@ -79,38 +90,38 @@ func parsePayload(secret string, payload string, prefix string, key string) (str
 	payloadCookie := fmt.Sprintf("%s|%s", payloadPrefix, payloadContent)
 	signature := hmacSha1(secret, payloadCookie)
 	if !hmac.Equal([]byte(hmacSha1(secret, payloadSignature)), []byte(hmacSha1(secret, signature))) {
-		return "", fmt.Errorf("signature hash does not match")
+		return "", errors.New(errParsePayloadInvalidSignature)
 	}
 
 	// Validate payload prefix
 	if payloadPrefix != prefix {
-		return "", fmt.Errorf("payload prefix does not match")
+		return "", errors.New(errParsePayloadInvalidPrefix)
 	}
 
 	// Validate payload content (Base64)
 	decodedContent, err := base64.StdEncoding.DecodeString(payloadContent)
 	contentValues := strings.Split(string(decodedContent), "|")
 	if err != nil || len(contentValues) != 3 {
-		return "", fmt.Errorf("payload content is invalid")
+		return "", errors.New(errParsePayloadInvalidContent)
 	}
 
 	// Validate payload expiration
 	timestamp := time.Now().Local().Unix()
 	payloadExpiration, err := strconv.ParseInt(contentValues[2], 10, 64)
 	if err != nil || payloadExpiration < timestamp {
-		return "", fmt.Errorf("payload expiration is invalid")
+		return "", errors.New(errParsePayloadInvalidExpiration)
 	}
 
 	// Validate payload key
 	payloadKey := contentValues[1]
 	if payloadKey != key {
-		return "", fmt.Errorf("payload key does not match")
+		return "", errors.New(errParsePayloadInvalidKey)
 	}
 
 	// Validate payload username
 	payloadUsername := contentValues[0]
 	if payloadUsername == "" {
-		return "", fmt.Errorf("payload username is invalid")
+		return "", errors.New(errParsePayloadInvalidUsername)
 	}
 
 	return payloadUsername, nil
--- duo/base_test.go
+++ duo/base_test.go
@@ -8,26 +8,111 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHmacSha1(t *testing.T) {
-	secret := "secret"
-	message := "message"
+var (
+	defaultUsername       = "tester"
+	defaultIntegrationKey = "abcdefghijklmnopqrst"
+	defaultApplicationKey = "abcdefghijklmnopqrstuvwxyz1234567890!@#$"
+	defaultSecretKey      = "abcdefghijklmnopqrstuvwxyz1234567890!@#$"
+	defaultHash           = "7402d46db372a8be0dcc3baa2570c87370055b44"
+	defaultSignature      = "TX|dGVzdGVyfGFiY2RlZmdoaWprbG1ub3BxcnN0fDk0NjY4NTA5OQ==|5e7b669b043b01060d56f86871abd1190a2491ec"
+	defaultEpochPast      = time.Date(1999, time.December, 31, 23, 59, 59, 999, time.UTC)
+	defaultEpochFuture    = time.Date(2099, time.December, 31, 23, 59, 59, 999, time.UTC)
+)
 
-	actual := hmacSha1(secret, message)
-	expected := "0caf649feee4953d87bf903ac1176c45e028df16"
-	assert.Equal(t, actual, expected)
+func TestHmacSha1WithDefaults(t *testing.T) {
+	actual := hmacSha1(defaultSecretKey, defaultUsername)
+	assert.Equal(t, defaultHash, actual)
 }
 
-func TestSignPayload(t *testing.T) {
-	death := time.Date(1999, time.December, 31, 23, 59, 59, 999, time.UTC)
-	patch := monkey.Patch(time.Now, func() time.Time { return death })
+func TestSignPayloadWithDefaults(t *testing.T) {
+	patch := monkey.Patch(time.Now, func() time.Time { return defaultEpochPast })
 	defer patch.Unpatch()
 
-	secret := "secret"
-	payload := []string{"username", "key"}
-	prefix := duoPrefix
-	ttl := duoExpiration
+	payload := []string{defaultUsername, defaultIntegrationKey}
+	actual := signPayload(defaultSecretKey, payload, duoPrefix, duoExpiration)
+	assert.Equal(t, defaultSignature, actual)
+}
 
-	actual := signPayload(secret, payload, prefix, ttl)
-	expected := "TX|dXNlcm5hbWV8a2V5fDk0NjY4NTA5OQ==|0047894ee8fc935b176b2cac2c57b358150471fc"
-	assert.Equal(t, actual, expected)
+func TestParsePayloadWithDefaults(t *testing.T) {
+	patch := monkey.Patch(time.Now, func() time.Time { return defaultEpochPast })
+	defer patch.Unpatch()
+
+	actual, _ := parsePayload(defaultSecretKey, defaultSignature, duoPrefix, defaultIntegrationKey)
+	assert.Equal(t, defaultUsername, actual)
+}
+
+func TestParsePayloadWithInvalidPayload(t *testing.T) {
+	patch := monkey.Patch(time.Now, func() time.Time { return defaultEpochPast })
+	defer patch.Unpatch()
+
+	payload := ""
+	_, err := parsePayload(defaultSecretKey, payload, duoPrefix, defaultIntegrationKey)
+	if assert.Errorf(t, err, "") {
+		assert.Equal(t, errParsePayloadInvalidPayload, err.Error())
+	}
+}
+
+func TestParsePayloadWithInvalidSignature(t *testing.T) {
+	patch := monkey.Patch(time.Now, func() time.Time { return defaultEpochPast })
+	defer patch.Unpatch()
+
+	payload := "TX|testerabcdefghijklmnopqrst946685099|"
+	_, err := parsePayload(defaultSecretKey, payload, duoPrefix, defaultIntegrationKey)
+	if assert.Errorf(t, err, "") {
+		assert.Equal(t, errParsePayloadInvalidSignature, err.Error())
+	}
+}
+
+func TestParsePayloadWithInvalidContent(t *testing.T) {
+	patch := monkey.Patch(time.Now, func() time.Time { return defaultEpochPast })
+	defer patch.Unpatch()
+
+	payload := "TX|testerabcdefghijklmnopqrst946685099|b3727c62edab9eaafa52ea8bc72a77c29d6e31fa"
+	_, err := parsePayload(defaultSecretKey, payload, duoPrefix, defaultIntegrationKey)
+	if assert.Errorf(t, err, "") {
+		assert.Equal(t, errParsePayloadInvalidContent, err.Error())
+	}
+}
+
+func TestParsePayloadWithInvalidPrefix(t *testing.T) {
+	patch := monkey.Patch(time.Now, func() time.Time { return defaultEpochPast })
+	defer patch.Unpatch()
+
+	payload := "NIL"
+	_, err := parsePayload(defaultSecretKey, defaultSignature, payload, defaultIntegrationKey)
+	if assert.Errorf(t, err, "") {
+		assert.Equal(t, errParsePayloadInvalidPrefix, err.Error())
+	}
+}
+
+func TestParsePayloadWithInvalidExpiration(t *testing.T) {
+	patch := monkey.Patch(time.Now, func() time.Time { return defaultEpochFuture })
+	defer patch.Unpatch()
+
+	_, err := parsePayload(defaultSecretKey, defaultSignature, duoPrefix, defaultIntegrationKey)
+	if assert.Errorf(t, err, "") {
+		assert.Equal(t, errParsePayloadInvalidExpiration, err.Error())
+	}
+}
+
+func TestParsePayloadWithInvalidKey(t *testing.T) {
+	patch := monkey.Patch(time.Now, func() time.Time { return defaultEpochPast })
+	defer patch.Unpatch()
+
+	payload := ""
+	_, err := parsePayload(defaultSecretKey, defaultSignature, duoPrefix, payload)
+	if assert.Errorf(t, err, "") {
+		assert.Equal(t, errParsePayloadInvalidKey, err.Error())
+	}
+}
+
+func TestParsePayloadWithInvalidUsername(t *testing.T) {
+	patch := monkey.Patch(time.Now, func() time.Time { return defaultEpochPast })
+	defer patch.Unpatch()
+
+	payload := "TX|fGFiY2RlZmdoaWprbG1ub3BxcnN0fDk0NjY4NTA5OQ==|3195c2bee68972654e1fd41c6e2f25ae584493cd"
+	_, err := parsePayload(defaultSecretKey, payload, duoPrefix, defaultIntegrationKey)
+	if assert.Errorf(t, err, "") {
+		assert.Equal(t, errParsePayloadInvalidUsername, err.Error())
+	}
 }
