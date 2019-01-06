c4b5a6fe155552760270e572f824766e836b1683
Rudy Puig <rudy.puig@boats.com>
2019-01-05 20:39:30 -0500

    Using fmt.Errorf instead of errors.New

--- duo/response.go
+++ duo/response.go
@@ -5,26 +5,36 @@ import (
 	"strings"
 )
 
+const (
+	errVerifyResponseInvalidResponse      = "received invalid server response"
+	errVerifyResponseInvalidAuthSignature = "failed to validate authentication signature"
+	errVerifyResponseInvalidAppSignature  = "failed to validate application signature"
+	errVerifyResponseInvalidUsername      = "usernames do not match"
+)
+
 func verifyResponse(integrationKey string, secretKey string, appKey string, prefix string, response string) (string, error) {
 	values := strings.Split(response, ":")
 	if len(values) != 2 {
-		return "", fmt.Errorf("received invalid server response")
+		return "", fmt.Errorf(errVerifyResponseInvalidResponse)
 	}
 
+	// Validate response authentication signature
 	authSignature := values[0]
 	authUsername, err := parsePayload(secretKey, authSignature, prefix, integrationKey)
 	if err != nil {
-		return "", fmt.Errorf("failed to validate authentication signature: %v", err)
+		return "", fmt.Errorf(errVerifyResponseInvalidAuthSignature)
 	}
 
+	// Validate response application signature
 	appSignature := values[1]
 	appUsername, err := parsePayload(appKey, appSignature, appPrefix, integrationKey)
 	if err != nil {
-		return "", fmt.Errorf("failed to validate application signature: %v", err)
+		return "", fmt.Errorf(errVerifyResponseInvalidAppSignature)
 	}
 
+	// Validate response usernames
 	if authUsername != appUsername {
-		return "", fmt.Errorf("username does not match")
+		return "", fmt.Errorf(errVerifyResponseInvalidUsername)
 	}
 
 	return authUsername, nil
